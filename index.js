c308088eeb838e956f9a519ed005fb0f4476dd20
rthomas86 <rebkthomas72@gmail.com>
2024-04-27 14:04:31 -0700

    Add mysql framework and connect to the database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const { employee_db } = require("./lib/Employee_Management");
 
 managementList = [
   { selection: "View All Employess" },
@@ -11,98 +12,8 @@ managementList = [
 ];
 
 /*
-// Without using async/await:
-inquirer.prompt({
-    type: 'input',
-    name: 'account',
-    message: 'What is the account?'
-}).then(answers => {
-    return getList().then(list => {
-        return inquirer.prompt(genList(list)).then( answers1 => {
-            return answers1.orders
-        })
-    })
-}).then(id => {
-    return inquirer.prompt(confirmUpdate(id))
-}).then(answers => {
-    // it's not easy to access the order id selected
-    if(answers.toUpdate) {
-        return 'to update'
-    } else {
-        return 'NOT to update'
-    }
-}).then(console.log)
-
-const genList = (list) => {
-  const choices = list.map((item, index) => {
-    return {
-      key: index,
-      name: `${item.id}: ${item.quantity}@${item.price}`,
-      value: item.id,
-    };
-  });
-  return {
-    type: "rawlist",
-    message: "Which order to pick",
-    name: "orders",
-    choices: choices,
-  };
-};
-
-const getList = () => {
-  return Promise.resolve([
-    {
-      id: "A001",
-      quantity: 20,
-      price: 103,
-    },
-    {
-      id: "A002",
-      quantity: 75,
-      price: 2.03,
-    },
-    {
-      id: "A003",
-      quantity: 16,
-      price: 900.01,
-    },
-  ]);
-};
-const confirmUpdate = (id) => {
-  return {
-    type: "confirm",
-    name: "toUpdate",
-    message: `Would you like to update ${id}?`,
-  };
-};
-*/
-
-/*
-// async/await awesomeness
-async function main() {
-  const getAccount = await inquirer.prompt({
-    type: "input",
-    name: "account",
-    message: "What is the account?",
-  });
-  const orderList = await getList();
-  const getOrder = await inquirer.prompt(genList(orderList));
-  const getConfirm = await inquirer.prompt(confirmUpdate(getOrder.orders));
-
-  if (getConfirm.toUpdate) {
-    console.log(
-      "to update",
-      getOrder.orders,
-      "for account",
-      getAccount.account
-    );
-    main();
-  } else {
-    console.log("NOT to update", getOrder.orders);
-  }
-}
 main();
-*/
+
 /*const mysql = require("mysql2");
 const db = mysql.createConnection(
   {
@@ -169,7 +80,8 @@ const questions = [
 //  use the Inquirer Promise to send the file name and entered data to the writeToFile method
 async function init() {
   const selection = await inquirer.prompt(makeList());
-  if (selection.transaction == 1) {
+  const db = new employee_db();
+  if (selection.transaction == 0) {
     console.log("here");
   }
 }
