d7482b55439309fd23b475c69e675a7fbcbc2f10
rthomas86 <rebkthomas72@gmail.com>
2024-04-28 08:44:01 -0700

    add view employees resultset

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
-const { employee_db, department, role } = require("./lib/Employee_Management");
+const {
+  employee_db,
+  department,
+  role,
+  employee,
+} = require("./lib/Employee_Management");
 
 managementList = [
   { selection: "View All Employess" },
@@ -12,48 +17,6 @@ managementList = [
   { selection: "Add Department" },
 ];
 
-/*
-main();
-
-/*const mysql = require("mysql2");
-const db = mysql.createConnection(
-  {
-    host: "localhost",
-    // MySQL username,
-    user: "root",
-    // MySQL password
-    password: "M0sfet~24",
-    database: "employee_db",
-  },
-  console.log(`Connected to the employee_db database.`)
-);
-
-// Query database
-db.query("SELECT * FROM role", function (err, results) {
-  let arrNoIndex = results.reduce((acc, { id, title, ...x }) => {
-    acc[title] = x;
-    return acc;
-  }, {});
-
-  console.table(arrNoIndex);
-  console.table(results);
-  console.log(results);
-});
-
-// Add the File System Package to create ad README.md file
-// Add the Inquirer Package to prompt the user for a Professional ReadMe at the console
-// Add my library to create the markdown for the ReadMe
-
-const fs = require("fs");
-const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
-*/
-
-// All of the inquirer prompts use the message attribute to ask the question and use the name attribute to store the result in a variable
-// Majority of the prompts are of the type input, where the user types in the information
-// The input for GitHub and email will be appended to the Questions Section
-// The type list will allow the user to select from a list the license they would like to use for their application
-// Inquirer will prompt the array of questions through a promise
 const makeList = () => {
   const choice = managementList.map((selection, index) => {
     return {
@@ -68,14 +31,6 @@ const makeList = () => {
     choices: choice,
   };
 };
-const questions = [
-  {
-    type: "list",
-    name: "management",
-    message: "What would you like to do? ",
-    choices: managementList,
-  },
-];
 
 // Initialize the program with the questions array
 //  use the Inquirer Promise to send the file name and entered data to the writeToFile method
@@ -83,13 +38,18 @@ async function init() {
   const selection = await inquirer.prompt(makeList());
   let departmentEntity;
   let roleEntity;
+  let employeeEntiy;
   let sql = "";
   let result;
   let params = {};
 
   if (selection.transaction == 0) {
-    console.log("here");
-    init();
+    employeeEntity = new employee();
+    sql = employeeEntity.viewRoles();
+    await employeeEntity.getData(sql, function (err, data) {
+      console.table(data);
+      init();
+    });
   } else if (selection.transaction == 1) {
     console.log("here");
   } else if (selection.transaction == 2) {
